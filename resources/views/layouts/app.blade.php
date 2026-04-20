f83a632296a6686ac49a9bc32cb42a687280ce96
FaturRosek <faturrosek0403@gmail.com>
2026-04-20 20:24:20 +0700

    Pipah bauk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../../assets/css/styles.min.css">
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/index.css">
 </head>
 
 <body>
